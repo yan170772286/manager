0b25df5ff809af8a6fb4fa4c3517628bd5cb9243
yanyan <yanyana@yonyou.com>
2018-01-24 10:37:18 +0800

    fx

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-$(function(){
-	alert('hehe')
-})
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+	$scope.a = {
+		title:"辽宁富昌隆大药房连锁企业经营项目管理有限公司调拨单"
+	}
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
